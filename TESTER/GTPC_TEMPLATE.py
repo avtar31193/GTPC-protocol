48381c46422d1fc70ec9c417908606afcbb597ef
avtar31193 <88155704+avtar31193@users.noreply.github.com>
2021-09-18 19:41:12 +0530

    Update GTPC_TEMPLATE.py

--- TESTER/GTPC_TEMPLATE.py
+++ TESTER/GTPC_TEMPLATE.py
@@ -1,48 +1,46 @@
 #!/usr/bin/python 
 
 ECHO_REQUEST = {
+	'Version':2,
+	'P':0,
+	'T':0,
 	'Spare1':0,
 	'Spare2':0,
-	'Spare3':0,
-	'T':0,
-	'P':0,
-	'Version':2,
-	'MessgaeType':1,
-	'MessageLength':13,
+	'Spare3':0,	
+	'MessageType':1,
+	'MessageLength':3+1,
 	'SequenceNumber':0,
-	'Spare4':0
+	'Spare4':0,
 	'IEs':{
 		'RECOVERY':{
 				'Type':3,
-				'Length':5,
+				'Length':1,
 				'Spare':0,
 				'Instance':0,
-				'Recovery':0,
-			},
-			'PRIVATE_EXTENSION':0
+				'IE_SPECIFIC_DATA':0
+		}
 	}	
 }
 
 ECHO_RESPONSE = {
+	'Version':2,
+	'P':0,
+	'T':0,
 	'Spare1':0,
 	'Spare2':0,
-	'Spare3':0,
-	'T':0,
-	'P':0,
-	'Version':2,
-	'MessgaeType':2,
-	'MessageLength':13,
+	'Spare3':0,	
+	'MessageType':2,
+	'MessageLength':3+1,
 	'SequenceNumber':0,
-	'Spare4':0
+	'Spare4':0,
 	'IEs':{
 		'RECOVERY':{
 				'Type':3,
-				'Length':5,
+				'Length':1,
 				'Spare':0,
 				'Instance':0,
-				'Recovery':0,
-		},
-		'CAUSE'=0
-		'PRIVATE_EXTENSION'=0
+				'IE_SPECIFIC_DATA':0
+		}
 	}	
 }
+
